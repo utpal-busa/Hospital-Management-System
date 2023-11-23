8a9c745c52c852e6d5a132993622748a27d7b7f0
Dipak478 <dipakbaghel82@gmail.com>
2023-11-24 00:40:47 +0530

    Updated Recep And Patient DashBoard and SideBar

---
+++ Backend/github_RegistrationForm/public/main.js
@@ -0,0 +1,134 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Date and time picker
+    $('.date').datetimepicker({
+        format: 'L'
+    });
+    $('.time').datetimepicker({
+        format: 'LT'
+    });
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: false,
+        animateOut: 'fadeOutLeft',
+        items: 1,
+        dots: true,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1000,
+        center: true,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    });
+
+    
+})(jQuery);
+
+// main.js
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     // Get the table body element
+//     const tableBody = document.getElementById('appointmentTableBody');
+  
+//     // Retrieve the appointments data from the backend (you can use a global variable or fetch API)
+//     const appointments = getAppointmentData(); // Update this line accordingly
+  
+//     // Loop through the appointments and populate the table dynamically
+//     appointments.forEach((appointment) => {
+//       const row = tableBody.insertRow();
+//       row.innerHTML = `
+//         <td>${appointment.SrNo}</td>
+//         <td>${appointment.Doctor}</td>
+//         <td>${appointment.AppointmentDate}</td>
+//         <td>${appointment.TimeSlot}</td>
+//         <td>${appointment.DoctorFee}</td>
+//         <td><a class="action-button" href="#">Edit</a></td>
+//       `;
+//     });
+//   });
+  
+//   // You need a function to fetch appointment data from the backend, using AJAX or fetch API
+//   function getAppointmentData() {
+//     // Implement fetching data from the backend and return the array of appointments
+//     // Example using fetch API:
+//     return fetch('/api/appointments')
+//       .then((response) => response.json())
+//       .then((data) => data.appointments)
+//       .catch((error) => {
+//         console.error('Error fetching appointment data:', error);
+//         return [];
+//       });
+//   }
+  
---
+++ Backend/github_RegistrationForm/src/db/conn.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose')
+mongoose.connect("mongodb://127.0.0.1:27017/software", { useNewUrlParser: true }, { useUnifiedTopology: true }, { useCreateIndex: true })
+    .then(() => { console.log("connection success") })
+    .catch((err) => { console.log(err) })
