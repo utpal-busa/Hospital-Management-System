3545428dcc469475736a94a3bf9e1ea90b5cc4e1
Dipak478 <94975180+Dipak478@users.noreply.github.com>
2023-12-09 11:11:33 +0530

    Update conn.js

--- Backend/github_RegistrationForm/src/db/conn.js
+++ Backend/github_RegistrationForm/src/db/conn.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://dipakbaghel82:iUUSrJguWfWEFqab@cluster0.i31dsqp.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true }, { useUnifiedTopology: true }, { useCreateIndex: true })
+mongoose.connect("contact dipak478", { useNewUrlParser: true }, { useUnifiedTopology: true }, { useCreateIndex: true })
     .then(() => { console.log("connection success") })
     .catch((err) => { console.log(err) })
